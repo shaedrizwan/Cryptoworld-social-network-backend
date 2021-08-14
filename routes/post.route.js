becb7f80cee12459fc795795e173dc0495bd8f0e
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-08-14 17:36:00 +0530

    Feat: Add post, get posts route added

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const { verifyAuth } = require('../middlewares/auth.middleware')
+const Post = require('../models/post.model')
 const router = express.Router()
 
+router.use(verifyAuth)
 router.route('/')
-    .get((req,res)=>{
-        res.json({success:true,message:"Post route"})
+    .get(async(req,res)=>{
+        try{
+            const posts = await Post.find({}).populate('user')
+            res.json({success:true,posts})
+        }catch(err){
+            res.json({success:false,error:err.message})
+        }
+    })
+
+router.route('/add')
+    .post(async(req,res)=>{
+        try{
+            const post = req.body
+            const addPost = new Post(post)
+            const addedPost = await addPost.save()
+            res.json({success:true,addedPost})
+        }catch(err){
+            res.json({success:false,error:err.message})
+        }
     })
 
 module.exports = router
