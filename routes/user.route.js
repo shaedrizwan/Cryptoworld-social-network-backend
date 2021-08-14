6073521cb5b134d73d882adc8306669b879b6427
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-08-14 17:34:09 +0530

    Feat: Login & Signup route added

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,36 @@
 const express = require('express')
+const User = require('../models/user.model')
 const router = express.Router()
+const jwt = require("jsonwebtoken")
+const bcrypt = require('bcryptjs')
+const { checkUser } = require('../middlewares/auth.middleware')
 
 router.route('/')
     .get((req,res)=>{
         res.json({success:true,message:"User route"})
+    })
+
+router.route('/signup')
+    .post(async(req,res)=>{
+        try{
+            const newUser = req.body
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(newUser.password,salt)
+            newUser.password = hashedPassword
+            const user = new User(newUser)
+            const addUser = await user.save()
+            res.json({success:true,addUser})
+        }catch(err){
+            res.json({success:false,message:err.message})
+        }
+    })
+
+router.use(checkUser)
+router.route('/login')
+    .post((req,res)=>{
+        const {userId} = req.user
+        const token = jwt.sign({userId},process.env.TOKEN_SECRET,{expiresIn:'24h'})
+        res.json({success:true,userId,token})
     })
 
 module.exports = router
